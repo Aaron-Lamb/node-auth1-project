85b5bb5d23dad58249da748e77c473ae613ee34f
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-10-28 16:27:39 -0700

    Index.js set up

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex');
+const database = require('./data/config');
+
+const server = express();
+const port = process.env.PORT || 4000;
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.use(session({
+    resave: false,
+    name: "Wookie",
+    saveUninitialized: false,
+    secret: process.env.SECRET,
+    store: new KnexSessionStore({
+        knex: database,
+        createTable: true
+    })
+}))
+
+server.use((err, req, res, next) => {
+    console.log(err);
+    return res.status(500).json({
+        errorMessage: "A server error occured"
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
